3ff27224722f80a3baba301f80575dab8d217791
TrueSaiyan <ufperth@protonmail.com>
2024-08-11 10:27:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv()
 API_ID = os.environ["API_ID"]
 API_HASH = os.environ["API_HASH"]
 SESSION = os.environ["SESSION"]
+TELETHON_SESSION = os.environ["TELETHON_SESSION"]
 FEDBAN_API_KEY = os.environ["FEDBAN_API_KEY"]
 GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 CMD_HANDLER = ["?"]
