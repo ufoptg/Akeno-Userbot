00ab8d196b67b64653b12710fab627bb42b1d4d8
TrueSaiyan <ufperth@protonmail.com>
2024-08-22 14:18:46 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 import os
+import nest_asyncio
 
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
 
-@app.get("/")
-def hello():
-    return {"message": "running"}
+app = FastAPI(
+title="PyroThon",
+version="1.0.0",
+contact={
+  "name": "Aᴋᴇɴᴏ",
+  "url": "https://github.com/ufoptg/Akeno-Userbot/",
+},
+docs_url=None, redoc_url="/"
+)
 
-if __name__ == "__main__":
+
+@app.get("/status")
+def status():
+    return {"message": "running"}
+
+if name == "main":
+    nest_asyncio.apply()
     uvicorn.run(app, host="0.0.0.0", port=7860)
